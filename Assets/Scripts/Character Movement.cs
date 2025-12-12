a3746db21ffdf031f839e2716e3630a68eb993ad
Michael Sawrin <sawrin.michael@googlemail.com>
2025-12-12 22:08:08 +0100

    Movement and Camera controls work with M&K and controller

--- Assets/Scripts/Character Movement.cs
+++ Assets/Scripts/Character Movement.cs
@@ -3,20 +3,12 @@ using UnityEngine.InputSystem;
 
 public class CharacterMovement : MonoBehaviour
 {
-    public InputActionAsset InputActions;
 
-    private InputAction moveAction;
-    private InputAction lookAction;
-    private InputAction jumpAction;
-
-    private Vector2 moveAmount;
-    private Vector2 lookAmount;
-
-    private Rigidbody rb;
-
+    [SerializeField] private Transform cameraTransform;
     [SerializeField] private float speed = 5f;
     [SerializeField] private float jumpHeight = 2f;
     [SerializeField] private float gravity = -9.8f;
+    [SerializeField] private bool shouldFaceMoveDirection = false;
 
     private CharacterController controller;
     private Vector3 moveInput;
@@ -47,8 +39,23 @@ public class CharacterMovement : MonoBehaviour
 
     void Update()
     {
-        Vector3 move = new Vector3(moveInput.x, 0, moveInput.y);
-        controller.Move(move*speed*Time.deltaTime);
+        Vector3 forward = cameraTransform.forward;
+        Vector3 right = cameraTransform.right;
+
+        forward.y = 0;
+        right.y = 0;
+
+        forward.Normalize();
+        right.Normalize();
+
+        Vector3 moveDirection = forward * moveInput.y + right * moveInput.x;
+        controller.Move(moveDirection * speed * Time.deltaTime);
+
+        if (shouldFaceMoveDirection && moveDirection.sqrMagnitude > 0.001f)
+        {
+            Quaternion toRotation = Quaternion.LookRotation(moveDirection, Vector3.up);
+            transform.rotation = Quaternion.Slerp(transform.rotation, toRotation, 10f * Time.deltaTime);
+        }
 
         velocity.y += gravity * Time.deltaTime;
         controller.Move(velocity * Time.deltaTime);
