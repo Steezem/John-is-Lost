7e2f43c2de21e55d1356e9dc61c71f71c7751158
Michael Sawrin <sawrin.michael@googlemail.com>
2025-12-23 19:01:22 +0100

    added check for player down vector3
    climbing of mountains is supposed to be prevented like this

--- Assets/Scripts/Character Movement.cs
+++ Assets/Scripts/Character Movement.cs
@@ -27,8 +27,12 @@ public class CharacterMovement : MonoBehaviour
 
     public void OnJump(InputAction.CallbackContext context)
     {
-
-        if (context.performed && controller.isGrounded)
+        // raycast hit is being used to check whether player is on more or less even ground
+        // climbing the terrain around the map is being made harder like this
+        // Player is not supposed to get outside of the mountain range 
+        RaycastHit hit;
+        Physics.Raycast(transform.position, -transform.up, out hit, 3f);
+        if (context.performed && controller.isGrounded && hit.normal.x < 0.1 && hit.normal.y > 0.8 && hit.normal.z < 0.1)
         {
             velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
         }
